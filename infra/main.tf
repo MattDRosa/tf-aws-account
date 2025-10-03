40c061a69c8bc15ebc9d55445cf17dab96ffc27f
Matheus Rosa <matheus202rs@gmail.com>
2025-10-02 21:34:41 -0300

    fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source = "./vpc"
 
-  vpc_name             = var.vpc_name
-  vpc_cidr             = var.vpc_cidr
-  public_subnets       = var.public_subnet_cidrs
-  private_subnets      = var.private_subnet_cidrs
+  vpc_name        = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
 }
