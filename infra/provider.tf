c2bbf8c106769014824130b89b659adab3752867
Matheus Rosa <matheus202rs@gmail.com>
2025-10-02 21:37:58 -0300

    fixed duplicated providers

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.11.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.93"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
