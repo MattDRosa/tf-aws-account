8d8ca0e300ac7b278a8eb0828c570c17819d9793
Matheus Rosa <matheus202rs@gmail.com>
2025-10-16 20:06:10 -0300

    count condition index fix

--- infra/iam/outputs.tf
+++ infra/iam/outputs.tf
@@ -1,15 +1,15 @@
 output "eks_cluster_role_arn" {
-  value = aws_iam_role.eks_cluster_role.arn
+  value = aws_iam_role.eks_cluster_role[count.index].arn
 }
 
 output "eks_master_role_name" {
-  value = aws_iam_role.eks_cluster_role.name
+  value = aws_iam_role.eks_cluster_role[count.index].name
 }
 
 output "eks_master_role_arn" {
-  value = aws_iam_role.eks_cluster_role.arn
+  value = aws_iam_role.eks_cluster_role[count.index].arn
 }
 
 output "eks_base_nodes_role_arn" {
-  value = aws_iam_role.eks_node_role.arn
+  value = aws_iam_role.eks_node_role[count.index].arn
 }
